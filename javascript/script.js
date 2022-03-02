9aecc3c63e8c15a324000eed49077424d3bac2c2
yooonsh <tjdgml991016@gmail.com>
2022-03-02 14:29:40 +0900

    배열

--- javascript/script.js
+++ javascript/script.js
@@ -8,8 +8,24 @@ console.log(fruit)
 fruit[2] = "tomato"
 console.log(fruit)
 
-//pop(): 마지막에 있는 아이템을 뺌
+//pop() : 마지막에 있는 아이템을 뺌
 fruit.pop()
 console.log(fruit)
+//push() : 배열값 추가
 fruit.push("pineapple")
+console.log(fruit)
+
+//includes : 아이템이 있는지 없는지 확인 true/false
+console.log(fruit.includes("pear")) 
+
+//indexOf : 아이템 인덱스 번호 알려줌
+console.log(fruit.indexOf("apple"))
+
+//slice : 배열 아이템을 잘라내는 역할 (시작점, 끝점-끝점 미포함), 기존배열 건드리지 않음
+let extraFruit = fruit.slice(1,3)
+console.log(extraFruit)
+console.log("original fruit", fruit)
+
+//splice : 배열 아이템을 잘라내는 역할 (시작점, 개수), 기존배열 건드림
+fruit.splice(2,1)
 console.log(fruit)
