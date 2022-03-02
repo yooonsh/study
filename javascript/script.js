beb7b8d6b3236c638874a1fb2625e61603a4b886
yooonsh <tjdgml991016@gmail.com>
2022-03-02 14:08:39 +0900

    Merge branch 'main' of https://github.com/yooonsh/study

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+let fruit = ["banana", "apple", "grape", "mango"]
+
+console.log(fruit)
+console.log(fruit[3])
+
+fruit[0] = "cherry"
+console.log(fruit)
+fruit[2] = "tomato"
+console.log(fruit)
+
+//pop(): 마지막에 있는 아이템을 뺌
+fruit.pop()
+console.log(fruit)
+fruit.push("pineapple")
+console.log(fruit)
