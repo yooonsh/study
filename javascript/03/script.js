3d46b409abe134627cd1f1b56cfbed6503ec4d5a
yooonsh <tjdgml991016@gmail.com>
2022-03-03 18:29:53 +0900

    1

--- javascript/03/script.js
+++ javascript/03/script.js
@@ -1,4 +1,23 @@
+console.log("==========================1번=============================")
+
 function greet(name) {
   console.log("안녕 내 이름은 ",name,"야")
 }
 greet("에밀리")
+
+console.log("==========================2번=============================")
+
+function greet(name) {
+  console.log("안녕 내 이름은 ",name,"야")
+}
+greet("에밀리")
+
+console.log("==========================3번=============================")
+
+function greet(name) {
+  console.log("안녕 내 이름은 ",name,"야")
+  return name
+}
+greet("에밀리")
+
+
