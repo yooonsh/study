658a2e43de8231412eb97849a4d27234a6fbb433
yooonsh <tjdgml991016@gmail.com>
2022-03-03 18:30:32 +0900

    3

--- javascript/03/script.js
+++ javascript/03/script.js
@@ -43,4 +43,23 @@ function meetAt(year, month, date) {
 console.log(meetAt(2022,1,7))
 
 
+console.log("==========================5번=============================")
+
+function findSmallestElement(arr) {
+	let result = arr[0];
+	if (arr.length === 0 ) {
+		return 0;
+	}
+  for (let i = 1;i < arr.length; i++){
+		if(result > arr[i]){
+			result = arr[i];
+		}
+	}
+	return result;
+}
+
+let smallestNumber = findSmallestElement([100,200,3,0,2,1])
+console.log(smallestNumber)
+
+
 
