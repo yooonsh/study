50d9eb1d577852b47ee77e17477ba3febf213450
ysh <sunghe1016@naver.com>
2022-03-04 11:31:34 +0900

    직접 코딩

--- javascript/03/script.js
+++ javascript/03/script.js
@@ -23,56 +23,45 @@ greet("에밀리")
 
 console.log("==========================4번=============================")
 
-function meetAt(year, month, date) {
-  let todayYear = year;
-  let todayMonth = month;
-  let todayDate = date;
-
-  if (todayDate) {
-    return `${todayYear}/${todayMonth}/${todayDate}`;
+function meetAt(year, month, date){
+  if(date){
+    return `${year}/${month}/${date}`
   }
-  if (todayMonth) {
-    return `${todayYear}년 ${todayMonth}월`;
+  if(month){
+    return `${year}년${month}월`
   }
-  if (todayYear) {
-    return `${todayYear}년`;
+  if(year){
+    return `${year}년`
   }
-
 }
-
-console.log(meetAt(2022,1,7))
+console.log(meetAt(2022,1,1))
 
 
 console.log("==========================5번=============================")
 
-function findSmallestElement(arr) {
-	let result = arr[0];
-	if (arr.length === 0 ) {
-		return 0;
-	}
-  for (let i = 1;i < arr.length; i++){
-		if(result > arr[i]){
-			result = arr[i];
-		}
-	}
-	return result;
+function findSmallestElement(arr){
+  if(arr.length == 0){
+    return 0
+  }
+  let result = arr[0]
+  for(i=1;i<arr.length;i++){
+    if(result > arr[i]){
+      result = arr[i]
+    }
+  }
+  return result
 }
-
-let smallestNumber = findSmallestElement([100,200,3,0,2,1])
-console.log(smallestNumber)
+console.log(findSmallestElement([100,200,3,0,2,1]))
 
 
 console.log("==========================6번=============================")
 
 let unit = [50000,10000,5000,1000,500,100]
-function changeCalculate(money) {
-  for(let i=0;i<unit.length;i++){
-    
-      let num = Math.floor(money / unit[i])
-      console.log(unit[i]+"X"+num)
-      money = money - (unit[i]*num)
-    
+function changeCalculate(money){
+  for(i=0;i<unit.length;i++){
+    let num = Math.floor(money / unit[i]) //Math.floor : 몫 나눌때 소수점 제거
+    money = money - (unit[i] * num)
+    console.log(`${unit[i]} X ${num}`)
   }
 }
-
-changeCalculate(12300)
+changeCalculate([12300])
