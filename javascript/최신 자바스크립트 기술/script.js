23845bc8c9c74b4ddef547a56dcb527e2448d27c
ysh <sunghe1016@naver.com>
2022-03-08 17:58:37 +0900

    배열함수 문제풀이 map~some

--- javascript/최신 자바스크립트 기술/script.js
+++ javascript/최신 자바스크립트 기술/script.js
@@ -1,3 +1,5 @@
+console.log("================ES6 최신문법=================")
+
 console.log("1. 다음 코드의 es6 문법을 이용하여 재작성 하시오")
 
 // let name="noona's fruit store"
@@ -140,3 +142,71 @@ function sumNumber() {
     return addNumber(1)(2)(3);
 }
 console.log(sumNumber())
+
+console.log("================배열함수=================")
+let names = [
+    "Steven Paul Jobs",
+    "Bill Gates",
+    "Mark Elliot Zuckerberg",
+    "Elon Musk",
+    "Jeff Bezos",
+    "Warren Edward Buffett",
+    "Larry Page",
+    "Larry Ellison",
+    "Tim Cook",
+    "Lloyd Blankfein",
+  ];
+
+console.log("1. map")
+  
+//모든 이름을 대문자로 바꾸어서 출력하시오.
+let data = names.map((item)=>{
+    return item.toUpperCase()
+}) 
+console.log(data)
+
+// /성을제외한 이름만 출력하시오. (예-[“Steven”,“Bill”,“Mark”,“Elon”…])
+let data1 = names.map((item)=>{
+    return item.split(' ')[0]//단어 자르기 split(자르는 기준 ex공백 또는 쉼표)[자를 배열]
+})
+console.log(data1)
+
+//이름의 이니셜만 출력하시오. (예-[“SPU”,“BG”,“MEZ”,“EM”…])
+let data2 = names.map((item)=>{
+    let splitName = item.split(" ")
+    let initial = ""
+    splitName.forEach((nameItem)=>(initial += nameItem[0]))
+    return initial
+})
+console.log(data2)
+
+console.log("2. filter")
+
+//이름에 a를 포함한 사람들을 출력하시오.
+let data3 = names.filter((item)=>{
+    return item.includes("a")
+})
+console.log(data3)
+
+//이름에 같은 글자가 연속해서 들어간 사람을 출력하시오. (예-tt,ff,ll 이런 글자들)
+let data4 =  names.filter((item) => {
+    let splitName = item.split("");
+    return splitName.some((letter, index) => letter == splitName[index + 1]);
+  })
+
+console.log(data4)
+
+console.log("3. some")
+
+//전체 이름의 길이가 20자 이상인 사람이 있는가?
+console.log(names.some((item)=>item.length>=20))
+
+//성을 제외한 이름에 p를 포함한 사람이 있는가?(대소문자 상관 no)
+let data5 = names.some((item)=>{
+    let splintName = item.split(' ')
+    splintName.pop()
+    return splintName.some((eachName)=>eachName.toLocaleLowerCase().includes('p')) //toLocaleLowerCase() 소문자로 변환
+})
+console.log(data5)
+
+
